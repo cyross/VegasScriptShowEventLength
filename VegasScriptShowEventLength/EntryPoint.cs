7e18a5662d41d802075b8d1fe77eb76dfb7f0dc2
Cyross Makoto <takuan45189@gmail.com>
2023-02-05 13:42:43 +0900

    first commit

---
+++ VegasScriptShowEventLength/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using ScriptPortal.Vegas;
+using VegasScriptHelper;
+using System;
+using System.Windows.Forms;
+
+namespace VegasScriptShowEventLength
+{
+    public class EntryPoint: IEntryPoint
+    {
+        public void FromVegas(Vegas vegas)
+        {
+            VegasHelper helper = VegasHelper.Instance(vegas);
+
+            try
+            {
+                Timecode time = helper.GetLengthFromAllEventsInTrack();
+                MessageBox.Show(time.ToString());
+            }
+            catch (VegasHelperTrackUnselectedException)
+            {
+                MessageBox.Show("ビデオトラックが選択されていません。");
+            }
+            catch (VegasHelperNoneEventsException)
+            {
+                MessageBox.Show("選択したビデオトラック中にイベントが存在していません。");
+            }
+        }
+    }
+}
